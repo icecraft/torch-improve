a146593c6b4bf998b1d7fa6d53c4e953cb8d832a
tmortred <tmortred@163.com>
2021-05-19 22:04:29 +0800

    1.init repo

---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+#include <torch/torch.h>
+
+#include <cstddef>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+auto main() -> int {
+
+    std::cout << "main" << std::endl;
+}
